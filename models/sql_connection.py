b7e5b18132391f08018bf0becd49f7d5a6b933dc
Gordi91 <szymon.pecuch@hotmail.com>
2018-11-02 10:22:27 +0100

    Active record design pattern User class implemented

---
+++ models/sql_connection.py
@@ -0,0 +1,17 @@
+from psycopg2 import connect
+
+
+def create_connection():
+    cnx = connect(user='postgres',
+                  password='coderslab',
+                  host='localhost',
+                  database='messenger')
+
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+    return cnx, cursor
+
+
+def close_connection(cnx, cursor):
+    cursor.close()
+    cnx.close()
